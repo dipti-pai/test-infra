5d9335cedb12447a4496aeb95418b2ed6f87f60d
Dipti Pai <diptipai89@outlook.com>
2024-07-19 13:42:00 -0700

    Add project_id to devops TF output
    Signed-off-by: Dipti Pai <diptipai89@outlook.com>

--- tf-modules/azure/devops/outputs.tf
+++ tf-modules/azure/devops/outputs.tf
@@ -2,3 +2,8 @@ output "repo_url" {
     description = "Azure Devops Git repository HTTPS url"
     value = azuredevops_git_repository.application.remote_url
 }
+
+output "project_id" {
+    description = "Azure Devops Project ID"
+    value = azuredevops_project.project.id  
+}
